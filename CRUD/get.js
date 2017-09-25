07801e64e957c2c289391c7f6dc85b6d811ec98d
samuele.malavasi <samuele.malavasi@gmail.com>
2017-09-25 18:34:12 +0200

    first commit

---
+++ CRUD/get.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.get = (event, context, callback) => {
+
+  var params = { };
+  params.TableName = process.env.MY_TABLE;
+  var key = {"Id": event.pathParameters.id};
+  params.Key = key;
+
+  // fetch todo from the database
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todo item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Item),
+    };
+    callback(null, response);
+  });
+};
---
+++ CRUD/list.js
@@ -0,0 +1,172 @@
+'use strict';
+
+var AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+var dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.list = (event, context, callback) => {
+ 
+  // Are there parameters ?
+  if(event.queryStringParameters != null){
+
+    var params = {
+      TableName: process.env.MY_TABLE,
+      FilterExpression:"",
+      ExpressionAttributeNames: {},
+      ExpressionAttributeValues: {}
+    };       
+
+    //Date interval
+    if(event.queryStringParameters.fromDate != null && event.queryStringParameters.toDate != null){
+      params.FilterExpression += "#Date BETWEEN :fromDate AND :toDate";
+
+      params.ExpressionAttributeNames["#Date"] = "Date";
+
+      params.ExpressionAttributeValues[":fromDate"] = parseInt(event.queryStringParameters.fromDate);
+      params.ExpressionAttributeValues[":toDate"] = parseInt(event.queryStringParameters.toDate);
+    }
+
+    //Readable Date interval
+    if(event.queryStringParameters.fromReadableDate != null && event.queryStringParameters.toReadableDate != null){
+      params.FilterExpression += "#Date BETWEEN :fromDate AND :toDate";
+
+      params.ExpressionAttributeNames["#Date"] = "Date";
+
+      params.ExpressionAttributeValues[":fromDate"] = getTimestampFromDate(event.queryStringParameters.fromReadableDate);
+      params.ExpressionAttributeValues[":toDate"] = getTimestampFromDate(event.queryStringParameters.toReadableDate);
+    }    
+
+    //Starting Altitude interval
+    if(event.queryStringParameters.fromStartingAltitude != null && event.queryStringParameters.toStartingAltitude != null){
+
+      if(params.FilterExpression != "")
+        params.FilterExpression += " AND ";
+
+      params.FilterExpression += "#StartingAltitude BETWEEN :fromStartingAltitude AND :toStartingAltitude";
+
+      params.ExpressionAttributeNames["#StartingAltitude"] = "StartingAltitude";
+
+      params.ExpressionAttributeValues[":fromStartingAltitude"] = parseInt(event.queryStringParameters.fromStartingAltitude);
+      params.ExpressionAttributeValues[":toStartingAltitude"] = parseInt(event.queryStringParameters.toStartingAltitude);      
+    }
+    
+    //Elevation gain interval
+    if(event.queryStringParameters.fromElevationGain != null && event.queryStringParameters.toElevationGain != null){
+      
+      if(params.FilterExpression != "")
+        params.FilterExpression += " AND ";
+
+      params.FilterExpression += "#ElevationGain BETWEEN :fromElevationGain AND :toElevationGain";
+
+      params.ExpressionAttributeNames["#ElevationGain"] = "ElevationGain";
+
+      params.ExpressionAttributeValues[":fromElevationGain"] = parseInt(event.queryStringParameters.fromElevationGain);
+      params.ExpressionAttributeValues[":toElevationGain"] = parseInt(event.queryStringParameters.toElevationGain);       
+    }
+    
+    //Grade
+    if(event.queryStringParameters.grade != null){
+      
+      if(params.FilterExpression != "")
+        params.FilterExpression += " AND ";
+
+      params.FilterExpression += "#Grade = :Grade";
+
+      params.ExpressionAttributeNames["#Grade"] = "Grade";
+
+      params.ExpressionAttributeValues[":Grade"] = event.queryStringParameters.grade;
+    }
+    
+    //Region
+    if(event.queryStringParameters.region != null){
+      
+      if(params.FilterExpression != "")
+        params.FilterExpression += " AND ";
+
+      params.FilterExpression += "contains(#Region, :Region)";
+
+      params.ExpressionAttributeNames["#Region"] = "Region";
+
+      params.ExpressionAttributeValues[":Region"] = event.queryStringParameters.region;      
+    }
+    
+    //UphillSide
+    if(event.queryStringParameters.uphillSide != null){
+      
+      if(params.FilterExpression != "")
+        params.FilterExpression += " AND ";
+
+      params.FilterExpression += "contains(#UphillSide, :UphillSide)";
+
+      params.ExpressionAttributeNames["#UphillSide"] = "UphillSide";
+
+      params.ExpressionAttributeValues[":UphillSide"] = event.queryStringParameters.uphillSide;           
+    }
+    
+    //DownhillSide
+    if(event.queryStringParameters.downhillSide != null){
+      
+      if(params.FilterExpression != "")
+        params.FilterExpression += " AND ";
+
+      params.FilterExpression += "contains(#DownhillSide, :DownhillSide)";
+
+      params.ExpressionAttributeNames["#DownhillSide"] = "DownhillSide";
+
+      params.ExpressionAttributeValues[":DownhillSide"] = event.queryStringParameters.downhillSide;          
+    }
+    
+    //TripName
+    if(event.queryStringParameters.tripName != null){
+      
+      if(params.FilterExpression != "")
+        params.FilterExpression += " AND ";
+
+      params.FilterExpression += "contains(#SearchTripName, :TripName)";
+
+      params.ExpressionAttributeNames["#SearchTripName"] = "SearchTripName";
+
+      params.ExpressionAttributeValues[":TripName"] = event.queryStringParameters.tripName;           
+    }
+
+  }
+  else{
+    var params = {
+      TableName: process.env.MY_TABLE
+    };       
+  }
+  
+  console.log("---------------------",params,"-------------------------");
+  dbScanFromParams(params, callback);
+
+};
+
+function dbScanFromParams(p, c){
+  dynamoDb.scan(p, (error, result) => {
+
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t fetch the todos.',
+      });
+      return;
+    }
+
+    var response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    };
+    c(null, response);
+
+  });    
+}
+
+function getTimestampFromDate(value){
+  value = value.match(/[0-9]*\/[0-9]*\/[0-9]*/gi)[0];
+  value = value.split("\/");
+  value = value[1]+","+value[0]+","+value[2];
+  return new Date(value).getTime();
+}
+
