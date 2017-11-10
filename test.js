ac7ae4b62d9012c51b21c94fbfa3642d2ca3617b
samuele.malavasi <samuele.malavasi@gmail.com>
2017-11-10 19:45:17 +0100

    aggiunto plugin per documentazione aws-gateway/swagger; aggiungo queryparams a funzione get per ricerca avanzata e bug fixes

--- test.js
+++ test.js
@@ -1,31 +1,27 @@
 'use strict';
-let https = require ('https');
 
-//curl 'https://api.qwant.com/api/search/images?count=50&q=montagne&t=images&safesearch=1&locale=en_US' -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Mobile Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Cache-Control: max-age=0' -H 'Connection: keep-alive' --compressed
+var AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+AWS.config.update({ region: 'eu-central-1' });
 
-let request_params = {
-	hostname : "api.qwant.com",
-	path : "/api/search/images?count=50&q=montagne&t=images&safesearch=1&locale=en_US",
-	headers : {
-		"Connection": "keep-alive",
-		"Accept": "application/json",
-		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0"
+
+var dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+var params = {
+	TableName: 'report-table',
+	IndexName: 'MyGsi',
+	//FilterExpression: '#Date >= :fromDate',
+	KeyConditionExpression: '#key = :value AND #Date >= :fromDate',
+	ExpressionAttributeNames: { '#key': 'Type', '#Date': 'Date' },
+	ExpressionAttributeValues: { ':value': 'ski-mountaineering', ':fromDate': 1510185600000 },
+	ScanIndexForward: false
+}
+
+dynamoDb.query(params, (error, result) => {
+	// handle potential errors
+	if (error) {
+		console.error(error);
+		return;
 	}
-};
 
-https.get(request_params, function (response) {
-	var body = '';
-
-	response.on('error', function (err) {
-		console.log("ERROR", err);
-	});
-
-	response.on('data', function (d) {
-		body += d;
-	});
-
-	response.on('end', function () {
-		var manna = JSON.parse (body)
-		callback(null, body);//manna.data.result.items);
-	});
+	console.log(JSON.stringify(result, null, 2));
 });
