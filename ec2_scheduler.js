cafa3c865264b895a3821a4b6742fdee1971ce07
samuele.malavasi <samuele.malavasi@gmail.com>
2017-11-15 18:42:56 +0100

    reso più elegante codice per ec2

--- ec2_scheduler.js
+++ ec2_scheduler.js
@@ -1,39 +1,28 @@
 'use strict';
 var AWS = require('aws-sdk');
+var aws_config = {region: process.env.REGION};
+
+var instances = {
+    InstanceIds : [process.env.INSTANCE_ID] 
+};
+
+var callback = function (err, data) {
+    if (err)
+        console.log(err, err.stack); // an error occurred
+    else
+        console.log(data); // successful response
+    
+    context.done(err,data);
+};
 
 module.exports.start = (event, context) => {
 
-    var ec2 = new AWS.EC2({
-        region: process.env.REGION
-    });
-    
-    ec2.startInstances({
-        InstanceIds : [process.env.INSTANCE_ID] 
-    },
-    function (err, data) {
-        if (err)
-            console.log(err, err.stack); // an error occurred
-        else
-            console.log(data); // successful response
-        
-        context.done(err,data);
-    });
+    var ec2 = new AWS.EC2(aws_config);
+    ec2.startInstances(instances,callback);
+
 };
 
 module.exports.stop = (event, context) => {
-    var ec2 = new AWS.EC2({
-        region: process.env.REGION
-    });
-    
-    ec2.stopInstances({
-        InstanceIds : [process.env.INSTANCE_ID] 
-    },
-    function (err, data) {
-        if (err)
-            console.log(err, err.stack); // an error occurred
-        else
-            console.log(data); // successful response
-        
-        context.done(err,data);
-    });
+    var ec2 = new AWS.EC2(aws_config);
+    ec2.stopInstances(instances,callback);
 };
